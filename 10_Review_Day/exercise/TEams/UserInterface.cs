ef3ff364f9ae7449908b6e23be7629946adb02dc
John Fulton <john@techelevator.com>
2022-01-27 17:58:23 -0500

    10_Review_Day/

---
+++ 10_Review_Day/exercise/TEams/UserInterface.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEams
+{
+    class UserInterface
+    {
+
+
+        public void Run()
+        {
+            // create some departments
+            CreateDepartments();
+
+            // print each department by name
+            PrintDepartments();
+
+            // create employees
+            CreateEmployees();
+
+            // give Angie a 10% raise, she is doing a great job!
+
+
+            // print all employees
+            PrintEmployees();
+
+            // create the TEams project
+            CreateTeamsProject();
+
+            // create the Marketing Landing Page Project
+            CreateLandingPageProject();
+
+            // print each project name and the total number of employees on the project
+            PrintProjectsReport();
+        }
+
+        /**
+         * Create departments and add them to the collection of departments
+         */
+        private void CreateDepartments()
+        {
+
+        }
+
+        /**
+         * Print out each department in the collection.
+         */
+        private void PrintDepartments()
+        {
+            Console.WriteLine("------------- DEPARTMENTS ------------------------------");
+
+        }
+
+        /**
+         * Create employees and add them to the collection of employees
+         */
+        private void CreateEmployees()
+        {
+
+        }
+
+        /**
+         * Print out each employee in the collection.
+         */
+        private void PrintEmployees()
+        {
+            Console.WriteLine("\n------------- EMPLOYEES ------------------------------");
+
+        }
+
+        /**
+         * Create the 'TEams' project.
+         */
+        private void CreateTeamsProject()
+        {
+
+        }
+
+        /**
+         * Create the 'Marketing Landing Page' project.
+         */
+        private void CreateLandingPageProject()
+        {
+
+        }
+
+        /**
+         * Print out each project in the collection.
+         */
+        private void PrintProjectsReport()
+        {
+            Console.WriteLine("\n------------- PROJECTS ------------------------------");
+
+        }
+    }
+}
